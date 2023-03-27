932685f4df6089f9747c00a3980d06ee77d16b91
Gabriel Van Zandycke <gabriel.vanzandycke@hotmail.com>
2023-03-27 18:29:43 +0200

    working version

--- running-with-bunnies/solution.py
+++ running-with-bunnies/solution.py
@@ -1,31 +1,31 @@
-import math
-
+# shortest path algorithm in a directed graph with negative edge weights
 def bellman_ford(times, s):
     N = len(times)
-    nodes = range(N)
-    distance = [math.inf]*N
-    path = [[] for _ in nodes]
-    distance[s] = 0
-    for i in range((N*(N-1))//2, 0, -1):
-        for x in nodes:
-            for y in nodes:
+    distances = [None]*N
+    path = [[] for _ in distances]
+    distances[s] = 0
+    for i in range((N*(N-1))//2-1, 0, -1):
+        for x, xdist in enumerate(distances):
+            if xdist is None:
+                continue
+            for y, ydist in enumerate(distances):
                 w = times[x][y]
-                if distance[x] + w < distance[y]:
+                if  ydist is None or xdist + w < ydist:
                     if i == 1:
-                        return None
-                    distance[y] = distance[x] + w
+                        raise
+                    distances[y] = xdist + w
                     path[y].append(x)
-    return distance, path
+    return distances, path
 
 def solution(times, time_limit):
     N = len(times)
     visited = set()
     missing = set(range(1, N-1))
     nodes = range(N)
-    output = zip(*[bellman_ford(times, s) for s in nodes])
-    if output is None:
+    try:
+        times, paths = zip(*[bellman_ford(times, s) for s in nodes])
+    except:
         return [n-1 for n in missing]
-    times, paths = output
     
     start = 0
     bulkhead = N-1
@@ -39,7 +39,4 @@ def solution(times, time_limit):
         time_limit -= times[start][node]
         start = node
     return [n-1 for n in visited]
-
-assert solution([[0, 2, 2, 2, -1], [9, 0, 2, 2, -1], [9, 3, 0, 2, -1], [9, 3, 2, 0, -1], [9, 3, 2, 2, 0]], 1) == [1, 2]
-assert solution([[0, 1, 1, 1, 1], [1, 0, 1, 1, 1], [1, 1, 0, 1, 1], [1, 1, 1, 0, 1], [1, 1, 1, 1, 0]], 3) == [0, 1]
 
