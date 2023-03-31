016355c6092b79c766c235fec2130f397989c8b6
Gabriel Van Zandycke <gabriel.vanzandycke@hotmail.com>
2023-03-31 15:11:39 +0200

    add test

--- elevator-maintenance/solution.py
+++ elevator-maintenance/solution.py
@@ -2,3 +2,4 @@ def solution(l):
     return sorted(l, key=lambda val: [int(section) for section in val.split(".")])
 
 assert(solution(["1.1.2", "1.0", "1.3.3", "1.0.12", "1.0.2"]) == ["1.0", "1.0.2", "1.0.12", "1.1.2", "1.3.3"])
+assert(solution(["1.1.2", "1", "1.0", "1.3.3", "1.0.12", "1.0.2"]) == ["1", "1.0", "1.0.2", "1.0.12", "1.1.2", "1.3.3"])
