0e0d5aef05623a9c9f4531ba95a6683216d81879
Gabriel Van Zandycke <gabriel.vanzandycke@hotmail.com>
2023-04-01 10:55:52 +0200

    solution + tests for ion-flux-relabelling

---
+++ ion-flux-relabelling/solution.py
@@ -0,0 +1,19 @@
+#                     31
+#       15                            30
+#   7          14             22              29
+# 3   6    10     13      18      21      25      28
+#1 2 4 5  8  9  11  12  16  17  19  20  23  24  26  27
+
+def solution(n):
+    if (n+1) & n == 0:      # top left tree:  n+1 is a power of 2
+        return (n << 1) + 1
+    if (n+2) & (n+1) == 0:  # top right tree: n+2 is a power of 2
+        return n+1
+    if n & (n-1) == 0:      # bottom right tree: n is a power of 2 (but not 1 or 2)
+        return n+2
+    p = 2**(n.bit_length()-1)-1
+    return solution((n & p) +1) + p
+
+tests = {1: 3, 2: 3, 3: 7, 4: 6, 5: 6, 6: 7, 7: 15, 8: 10, 9: 10, 10: 14, 11: 13, 12: 13, 13: 14, 14: 15, 15: 31, 16: 18, 17: 18, 18: 22, 19: 21, 20: 21, 21: 22, 22: 30, 23: 25, 24: 25, 25: 29, 26: 28, 27: 28, 28: 29, 29: 30, 30: 31}
+for n, s in tests.items():
+    assert solution(n) == s, f"Failed for solution({n}) == {s}. Received {solution(n)}"
